a7ba7084daec650f1091ca4f27dbd3705e15d00d
Ronivasconcelos <95322796+Ronivasconcelos@users.noreply.github.com>
2026-08-23 08:32:28 -0300

    Delete electron directory

--- electron/preload.cjs
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  isDesktop: true,
-  printPdf: (html) => ipcRenderer.invoke('print-pdf', html),
-  exportBackup: () => ipcRenderer.invoke('export-backup')
-});
